80873b1846f00b617255111ab055b9688349e569
marun790 <marun790@gmail.com>
2020-02-13 09:09:57 +0530

    useEffect/mimick componentdidmount

---
+++ src/components/HookMouse.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+
+export default function HookMouse() {
+
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+
+    const logMousePosition = e => {
+        console.log("logMousePosition");
+        setX(e.clientX);
+        setY(e.clientY);
+    }
+
+    useEffect(() => {
+        console.log("useEffect called");
+        window.addEventListener("mousemove", logMousePosition);
+    }, []); 
+   /**   by pasing an empty array we mimik the component did mount, 
+    so the useEffect() wont be called whenever the state got changed.*/
+
+
+    return (
+        <div>
+            X : {x} And Y : {y}
+        </div>
+    )
+}
